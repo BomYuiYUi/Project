307b3d0f24b5a4ab480fa931d69c08d66cbacdb7
User <User@LAPTOP-UIF87597>
2023-04-09 14:18:33 +0700

    addtoCartFunction

--- src/app/page/productdetail/productdetail.component.ts
+++ src/app/page/productdetail/productdetail.component.ts
@@ -1,20 +1,73 @@
 import { Component,OnInit } from '@angular/core';
 import { DataServiceService } from 'src/app/data-service.service';
 import { ActivatedRoute} from '@angular/router';
+import { AuthService } from 'src/app/service/auth.service';
+import { Router } from '@angular/router';
+import { FormGroup, FormBuilder} from '@angular/forms';
+import { CartService } from 'src/app/service/cart.service';
+import { userdetail } from 'src/app/model/userdetail';
 
 @Component({
   selector: 'app-productdetail',
   templateUrl: './productdetail.component.html',
   styleUrls: ['./productdetail.component.css']
 })
-export class ProductdetailComponent implements OnInit{
+export class ProductdetailComponent{
+  addForm!: FormGroup;
   myProduct: any={};
+  myUser: userdetail;
   constructor(
     public actRoute : ActivatedRoute,
     public dts : DataServiceService,
+    private authService: AuthService,
+    private cartService: CartService,
+    private router: Router,
+    private formBuilder: FormBuilder
   ){}
   id = this.actRoute.snapshot.params['id']
   ngOnInit(): void {
     this.dts.getProductOne(this.id).subscribe((data)=>{this.myProduct=data})
+
+    this.addForm = this.formBuilder.group({
+      qty: ['']
+    });
+    const token = localStorage.getItem('token');
+    if (token) {
+      this.authService.getAuth(token).subscribe(
+        (data)=>{this.myUser=data},
+        (error) => {
+          console.log(error);
+          alert(error.error);
+          localStorage.removeItem('token');
+          this.router.navigate(['/login']);
+        }
+      );
+    }
+    else{
+      this.router.navigate(['/login']);
+    }
+  }
+  onSubmit() {
+    console.log("myUser:", this.myUser);
+    console.log("myProduct:", this.myProduct);
+    const { qty } = this.addForm.value;
+    console.log("qty:", qty);
+    const newprice = qty * this.myProduct.ProPrice;
+    console.log("newprice:", newprice);
+    if (typeof qty !== 'number' || qty <= 0) {
+      alert('Invalid quantity');
+      return;
+    }
+    this.cartService.addProduct(this.myUser.username,this.myProduct.ProID,this.myProduct.ProImg, this.myProduct.ProName,qty,newprice).subscribe({
+      next:(product)=>{
+        console.log('add to carts')
+      },
+      error:(response)=>{
+        console.log(response)
+      },
+      complete:()=>{
+        alert('dddddddd')
+      }
+    });
   }
 }
